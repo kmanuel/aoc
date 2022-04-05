23347ddbb6e9658aa68be4710050da0a2541d2d9
kmanuel <kruisz88@gmail.com>
2022-04-05 14:20:07 +0200

    2020 day13 p2

--- 2020/13/index.ts
+++ 2020/13/index.ts
@@ -27,3 +27,33 @@ function solve1(file: string) {
 
 solve1('sample.txt')
 solve1('input.txt')
+
+function solve2(file: string) {
+  const [start, busesLine] = fs.readFileSync(file, 'utf-8').split('\n')
+  let busParts = busesLine.split(',')
+  let buses: { offset: number; id: number }[] = []
+  for (let i = 0; i < busParts.length; i++) {
+    if (busParts[i] === 'x') {
+      continue
+    }
+    buses.push({ offset: i, id: Number(busParts[i]) })
+  }
+
+  let time = buses[0].id
+  let stepSize = buses[0].id
+
+  for (let bus of buses.slice(1)) {
+    while ((bus.offset + time) % bus.id !== 0) {
+      time += stepSize
+    }
+    stepSize *= bus.id
+  }
+
+  console.log(time)
+}
+
+solve2('sample.txt')
+solve2('sample1.txt')
+solve2('sample2.txt')
+solve2('sample3.txt')
+solve2('input.txt')
